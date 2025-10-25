1953e69390d213d130b56c698796ab8c2fc871d5
Akshatvijesh26 <9e26.akshatvijesh@gmail.com>
2025-10-25 12:32:54 +0530

    Add files via upload

---
+++ newsportal/myapp/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Article, Category
+
+admin.site.register(Article)
+admin.site.register(Category)
+
+#http://127.0.0.1:8000/#
+
+
+#http://127.0.0.1:8000/admin/#
---
+++ newsportal/myapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import Article, Category
+
+def home(request):
+    articles = Article.objects.order_by('-published_at')
+    categories = Category.objects.all()  # fetch all categories
+    return render(request, 'myapp/home.html', {
+        'articles': articles,
+        'categories': categories
+    })
+
+def category_articles(request, category_id):
+    articles = Article.objects.filter(category_id=category_id).order_by('-published_at')
+    categories = Category.objects.all()
+    return render(request, 'myapp/home.html', {
+        'articles': articles,
+        'categories': categories
+    })
+
